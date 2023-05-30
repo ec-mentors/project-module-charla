a8791cc1c67ac33491d953c985c08d0e0d838dbe
Ali <onynari@gmail.com>
2023-05-30 14:04:22 +0200

    created project

---
+++ login-manager/src/test/java/io/charla/login_manager/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.charla.login_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
