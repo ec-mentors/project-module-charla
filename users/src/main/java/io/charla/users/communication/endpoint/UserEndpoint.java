ecef4b7fc982ceb2ac6dcacd05b37e3394db4734
Ali <onynari@gmail.com>
2023-05-30 15:52:16 +0200

    user login verification

--- users/src/main/java/io/charla/users/communication/endpoint/UserEndpoint.java
+++ users/src/main/java/io/charla/users/communication/endpoint/UserEndpoint.java
@@ -1,4 +1,19 @@
 package io.charla.users.communication.endpoint;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class UserEndpoint {
+
+
+    @GetMapping("/users")
+    public String loginUser() {
+
+        return "logged in successfully";
+    }
+
+
 }
--- users/src/main/java/io/charla/users/security/SecurityConfig.java
+++ users/src/main/java/io/charla/users/security/SecurityConfig.java
@@ -1,4 +1,40 @@
 package io.charla.users.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors().and().csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .authenticationEntryPoint(authenticationEntryPoint());
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationEntryPoint authenticationEntryPoint() {
+        return (request, response, authException) -> {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("application/json");
+
+            String responseBody = "{\"Error\":\"Please make sure you have entered correct credentials!\"}";
+            PrintWriter writer = response.getWriter();
+            writer.print(responseBody);
+            writer.flush();
+        };
+    }
 }
